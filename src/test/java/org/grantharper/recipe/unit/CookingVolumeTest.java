58d7d0fa0856dec3ef25fb26cbb31b52e26e792f
grantharper <gharper3@gmail.com>
2019-05-19 13:02:48 -0400

    write test to scale a list of ingredients

--- src/test/java/org/grantharper/recipe/unit/CookingVolumeTest.java
+++ src/test/java/org/grantharper/recipe/unit/CookingVolumeTest.java
@@ -1,11 +1,13 @@
 package org.grantharper.recipe.unit;
 
-
 import org.junit.Test;
 import tec.units.ri.quantity.Quantities;
 
 import javax.measure.Quantity;
 import javax.measure.quantity.Volume;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.grantharper.recipe.unit.CookingUnits.*;
@@ -14,7 +16,6 @@ public class CookingVolumeTest {
 
     @Test
     public void exploreTeaspoonAndTablespoon() {
-
 
         Quantity<Volume> sugar = Quantities.getQuantity(3, TEASPOON);
 
@@ -67,7 +68,29 @@ public class CookingVolumeTest {
         assertThat(tbsp.getValue()).isEqualTo(4.0);
     }
 
+    @Test
+    public void testScalePizzaDoughRecipe() {
+        Quantity<Volume> wheatFlour = Quantities.getQuantity(1, CUP);
+        Quantity<Volume> allPurposeFlour = Quantities.getQuantity(2, CUP);
+        Quantity<Volume> salt = Quantities.getQuantity(0.5, TEASPOON);
+        Quantity<Volume> oliveOil = Quantities.getQuantity(0.25, CUP);
+        Quantity<Volume> sugar = Quantities.getQuantity(1, TEASPOON);
+        Quantity<Volume> activeDryYeast = Quantities.getQuantity(1, TABLESPOON);
+        Quantity<Volume> water = Quantities.getQuantity(1, CUP);
 
+        List<Quantity<Volume>> ingredients = List.of(wheatFlour, allPurposeFlour, salt, oliveOil, sugar, activeDryYeast, water);
+
+        List<Quantity<Volume>> scaledIngredients = ingredients.stream().map(volumeQuantity -> volumeQuantity.divide(2)).collect(Collectors.toList());
+
+        assertThat(scaledIngredients.get(0).getValue()).isEqualTo(0.5);
+        assertThat(scaledIngredients.get(1).getValue()).isEqualTo(1.0);
+        assertThat(scaledIngredients.get(2).getValue()).isEqualTo(0.25);
+        assertThat(scaledIngredients.get(3).getValue()).isEqualTo(0.125);
+        assertThat(scaledIngredients.get(4).getValue()).isEqualTo(0.5);
+        assertThat(scaledIngredients.get(5).getValue()).isEqualTo(0.5);
+        assertThat(scaledIngredients.get(6).getValue()).isEqualTo(0.5);
+
+    }
 
 
 
